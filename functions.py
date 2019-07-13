f1bb7795949e3affc1402d4b654291a91adb547f
qiaowenz <niexu71@gmail.com>
2019-07-13 16:49:46 +0800

    new function

--- functions.py
+++ functions.py
@@ -2,17 +2,22 @@ students = []
 def get_student_titlecase():
     students_titlecase=[]
     for student in students:
-        students_titlecase=student.title()
+        students_titlecase=student["name"].title()
     return students_titlecase
 
 def print_student_titlecase():
     students_titlecase=[]
-    for student in students:
-        students_titlecase=student.title()
+    students_titlecase=get_student_titlecase()
     print(students_titlecase)
 
-def add_student(name):
-    students.append(name)
+def add_student(name, student_id=332):
+    student={"name":name, "student_id":student_id}
+    students.append(student)
 
 student_list = get_student_titlecase()
+student_name = input("Enter student name:")
+student_id = input("Enter student id:")
+
+add_student(student_name, student_id)
+print_student_titlecase()
 
